e609528d3f1e639ec4a3a9cf2b94e43f8d446ba3
Mário da Costa Lima Neto <mc0sta7220@gmail.com>
2025-10-06 22:17:06 -0300

    mudança

--- exercicios_logica/numeros_primos.py
+++ exercicios_logica/numeros_primos.py
@@ -1,37 +1,74 @@
 '''Numeros primos entre 10 e 20'''
-def numeros_primos_entre_10_e_20():  
-    numeros_primos = []
+# def numeros_primos_entre_10_e_20():  
+#     numeros_primos = []
 
-    for contagem in range(10,21):
-        if (contagem % 2 != 0 and contagem % 3 != 0 and contagem % 4 != 0 and contagem % 5 != 0 and contagem % 6 != 0 and
-            contagem % 7 != 0 and contagem % 8 != 0 and contagem % 9 != 0):
-            numeros_primos.append(contagem)
+#     for contagem in range(10,21):
+#         if (contagem % 2 != 0 and contagem % 3 != 0 and contagem % 4 != 0 and contagem % 5 != 0 and contagem % 6 != 0 and
+#             contagem % 7 != 0 and contagem % 8 != 0 and contagem % 9 != 0):
+#             numeros_primos.append(contagem)
 
-    return numeros_primos
+#     return numeros_primos
 
-def numero_primo(numero):
+# def numero_primo(numero):
     
-    if numero < 2:
-        return 'Não é primo'
+#     if numero < 2:
+#         return 'Não é primo'
     
-    elif numero == 2:
-        return 'O número é primo'
+#     elif numero == 2:
+#         return 'O número é primo'
     
-    elif numero & 2 == 0:
-        return 'O número não é primo'
+#     elif numero & 2 == 0:
+#         return 'O número não é primo'
     
-    for numerais in range(3,int(numero**0.5)+1,2):
-        if numero % numerais == 0:
-            return 'O número é primo'
-    else:
-        return 'Não é primo'
-    
-print(numeros_primos_entre_10_e_20())
+#     for numerais in range(3,int(numero**0.5)+1,2):
+#         if numero % numerais == 0:
+#             return 'O número é primo'
+#     else:
+#         return 'Não é primo'
 
-'''Teste para verificar numero primo aleatório'''
-while True:
-    try:
-        numero = int(input('Nº: '))
-        print(numero_primo(numero))
-    except ValueError:
-        print('Digite apenas números!')
+# while True:
+#     try:
+#         numero = int(input('Nº: '))
+#         print(numero_primo(numero))
+#     except ValueError:
+#         print('Digite apenas números!')
+# for n in range(2,101):
+#     if n in (2, 3, 5, 7):
+#         print(n , 'é primo')
+#     elif (n % 2 != 0 and n % 3 != 0 and n % 4 != 0 and n % 5 != 0 and
+#         n % 6 != 0 and n % 7 != 0 and n % 8 != 0 and n % 9 != 0 and n % 10 != 0):
+#         print(n, 'é primo')
+
+
+# ### Fatorial de um número
+# n = int(input())
+# f = 1
+# for i in range(1, n+1):
+#     f *= i
+# print(f) 
+
+### SOBRECARGA DE OPERADORES
+# https://realpython.com/operator-function-overloading/
+
+## funções recursivas https://www.dio.me/articles/funcoes-recursivas-em-python-o-que-sao-e-como-funcionam
+# number = int(input("Digite um número: "))
+# def factorial_rec(number: int) -> int:
+#     if number == 1:
+#         return 1
+#     return number * factorial_rec(number - 1)
+
+# print(factorial_rec(number))
+
+def eh_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+a = int(input())
+b = int(input())
+for num in range(a, b+1):
+    if eh_primo(num):
+        print(num)
--- exercicios_logica/palindromo.py
+++ exercicios_logica/palindromo.py
@@ -1,42 +1,60 @@
-frase = 'a grama é amarga'
+frase = 'Socorram-me, subi no ônibus em Marrocos'
 
-frase_lower = frase.lower()
+# frase_lower = frase.lower()
 
-frase_sem_barras = frase_lower.replace(' ','')
-frase_sem_travessao = frase_sem_barras.replace('-','')
-frase_sem_virgulas = frase_sem_travessao.replace(',','')
-frase_sem_ponto_final = frase_sem_virgulas.replace('.','')
-frase_sem_exclamacao = frase_sem_ponto_final.replace('!','')
-frase_sem_interrogacao = frase_sem_exclamacao.replace('?','')
-frase_sem_dois_pontos = frase_sem_interrogacao.replace(':','')
-frase_sem_ponto_e_virgula = frase_sem_dois_pontos.replace(';','')
-frase_sem_underline = frase_sem_ponto_e_virgula.replace('_','')
+# frase_sem_barras = frase_lower.replace(' ','')
+# frase_sem_travessao = frase_sem_barras.replace('-','')
+# frase_sem_virgulas = frase_sem_travessao.replace(',','')
+# frase_sem_ponto_final = frase_sem_virgulas.replace('.','')
+# frase_sem_exclamacao = frase_sem_ponto_final.replace('!','')
+# frase_sem_interrogacao = frase_sem_exclamacao.replace('?','')
+# frase_sem_dois_pontos = frase_sem_interrogacao.replace(':','')
+# frase_sem_ponto_e_virgula = frase_sem_dois_pontos.replace(';','')
+# frase_sem_underline = frase_sem_ponto_e_virgula.replace('_','')
 
-frase_sem_acento_a_circunflexo = frase_sem_ponto_e_virgula.replace('â','')
-frase_sem_acento_e_circunflexo = frase_sem_acento_a_circunflexo.replace('ê','')
-frase_sem_acento_i_circunflexo = frase_sem_acento_e_circunflexo.replace('î','')
-frase_sem_acento_o_circunflexo = frase_sem_acento_i_circunflexo.replace('ô','')
-frase_sem_acento_u_circunflexo = frase_sem_acento_o_circunflexo.replace('û','')
+# frase_sem_acento_a_circunflexo = frase_sem_ponto_e_virgula.replace('â','')
+# frase_sem_acento_e_circunflexo = frase_sem_acento_a_circunflexo.replace('ê','')
+# frase_sem_acento_i_circunflexo = frase_sem_acento_e_circunflexo.replace('î','')
+# frase_sem_acento_o_circunflexo = frase_sem_acento_i_circunflexo.replace('ô','')
+# frase_sem_acento_u_circunflexo = frase_sem_acento_o_circunflexo.replace('û','')
 
-frase_sem_acento_a_agudo = frase_sem_acento_u_circunflexo.replace('á','')
-frase_sem_acento_e_agudo = frase_sem_acento_a_agudo.replace('é','')
-frase_sem_acento_i_agudo = frase_sem_acento_e_agudo.replace('í','')
-frase_sem_acento_o_agudo = frase_sem_acento_i_agudo.replace('ó','')
-frase_sem_acento_u_agudo = frase_sem_acento_o_agudo.replace('ú','')
+# frase_sem_acento_a_agudo = frase_sem_acento_u_circunflexo.replace('á','')
+# frase_sem_acento_e_agudo = frase_sem_acento_a_agudo.replace('é','')
+# frase_sem_acento_i_agudo = frase_sem_acento_e_agudo.replace('í','')
+# frase_sem_acento_o_agudo = frase_sem_acento_i_agudo.replace('ó','')
+# frase_sem_acento_u_agudo = frase_sem_acento_o_agudo.replace('ú','')
 
-frase_sem_tio_a = frase_sem_acento_u_agudo.replace('ã','')
-frase_sem_tio_o = frase_sem_tio_a.replace('õ','')
+# frase_sem_tio_a = frase_sem_acento_u_agudo.replace('ã','')
+# frase_sem_tio_o = frase_sem_tio_a.replace('õ','')
 
-frase_sem_crase_a = frase_sem_tio_o.replace('à','')
-frase_sem_crase_e = frase_sem_crase_a.replace('è','')
-frase_sem_crase_i = frase_sem_crase_e.replace('ì','')
-frase_sem_crase_o = frase_sem_crase_i.replace('ò','')
-frase_sem_crase_u = frase_sem_crase_o.replace('ù','')
+# frase_sem_crase_a = frase_sem_tio_o.replace('à','')
+# frase_sem_crase_e = frase_sem_crase_a.replace('è','')
+# frase_sem_crase_i = frase_sem_crase_e.replace('ì','')
+# frase_sem_crase_o = frase_sem_crase_i.replace('ò','')
+# frase_sem_crase_u = frase_sem_crase_o.replace('ù','')
 
-frase1 = frase_sem_acento_u_circunflexo
-frase2 = frase1[::-1]
+# frase1 = frase_sem_acento_u_circunflexo
+# frase2 = frase1[::-1]
 
-if frase2 == frase1:
-    print('É palíndromo')
+# if frase2 == frase1:
+#     print('É palíndromo')
+# else:
+#     print('Não é palíndromo')
+
+for caracter in ['-', '/', ',']:
+    frase.replace('', caracter)
+
+    
+import unicodedata
+
+def remover_acentos(txt):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', txt)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+frase_limpa = remover_acentos(frase.lower().replace(' ', '').replace(',', '').replace('-', ''))
+if frase_limpa == frase_limpa[::-1]:
+    print("É um palíndromo")
 else:
-    print('Não é palíndromo')
+    print("Não é um palíndromo")
