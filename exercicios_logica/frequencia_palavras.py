e79bea2e224d827d7d2e7f6f51136cda0a34a1b2
Mário da Costa Lima Neto <mc0sta7220@gmail.com>
2025-10-07 21:09:27 -0300

    desafio 1 concluido

--- exercicios_logica/frequencia_palavras.py
+++ exercicios_logica/frequencia_palavras.py
@@ -1,3 +1,12 @@
-frutas = 'banana maçã banana uva uva banana'
+from collections import Counter
 
-print(frutas.split())
+frutas = 'banana maçã banana uva uva banana maçã maçã maçã'
+frutas1 = frutas.split()
+
+quantidade_de_frutas = Counter(frutas1)
+frutas2 = dict(quantidade_de_frutas)
+
+frutas_em_ordem = sorted(frutas2.items(), key=lambda pares_defrutas: pares_defrutas[1],reverse=True)
+
+for fruits, quantidades in frutas_em_ordem:
+    print(f'{fruits}: {quantidades}')
