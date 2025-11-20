723d99921a980f59ad16202fc06ca879a566dbfc
Mário da Costa Lima Neto <mc0sta7220@gmail.com>
2025-11-20 20:02:53 -0300

    Update FIFA.py

--- exercicios_logica/FIFA.py
+++ exercicios_logica/FIFA.py
@@ -18,6 +18,8 @@ while repeticao != 'N':
     while jogador_aleatorio != condicao_de_parada:
 
             jogador_aleatorio = str(input('Nome do jogador: ')).lower()
+            if jogador_aleatorio == condicao_de_parada:
+                   break
             jogadores.append(jogador_aleatorio)
 
     print('  ---Times---  ' \
@@ -26,12 +28,9 @@ while repeticao != 'N':
     while time_aleatorio != condicao_de_parada:
 
             time_aleatorio = str(input('Nome do time: ')).lower()
+            if time_aleatorio == condicao_de_parada:
+                   break
             times.append(time_aleatorio)
-
-    if 'pare' in times:
-            times.remove('pare')
-    if 'pare' in jogadores:
-            jogadores.remove('pare')
 
     random.shuffle(jogadores)
     random.shuffle(times)
@@ -41,3 +40,20 @@ while repeticao != 'N':
     print(dict(final))
 
     repeticao = str(input('Deseja repetir? S/N: ')).upper()
+
+"""times_adicionados = []
+
+for i in range(2):
+
+   adcionar_times = str(input('Time a mais: '))
+   times_adicionados.append(adcionar_times)
+
+jogadores_a_mais = []
+
+for i in range(2):
+        jogadores_com_2_times = random.choice(jogadores)
+        jogadores_a_mais.append(jogadores_com_2_times)
+
+final = dict(zip(jogadores_a_mais, times_adicionados))
+
+print(final)"""
