8af967f061801f94bf4bad062cf3655cdef87e39
Mário da Costa Lima Neto <mc0sta7220@gmail.com>
2025-12-04 22:46:56 -0300

    Pessoas mais leves e mais pesadas
    +1 exercício fazendo uma filtragem de informações de pessoas. tudo com a estrutura de dados Lista :)

--- exercicios_logica/filtros.py
+++ exercicios_logica/filtros.py
@@ -71,3 +71,54 @@ if produto_mais_barato in produtos:
 nome_do_produto_mais_barato = nomes_dos_produtos[indice_produto_mais_barato]
 
 print(f'O produto mais barato é: {nome_do_produto_mais_barato}')
+
+
+# Pessoas mais leves e mais pesadas
+# lista
+pessoas = list()
+pessoa = list()
+
+for i in range(4):
+    pessoa.append(str(input('Nome: ')))
+
+    while True:
+        try:
+            pessoa.append(int(input('Idade: ')))
+            break
+        except ValueError:
+            print('Para idade, digite apenas números!')
+
+    pessoas.append(pessoa[:])
+    pessoa.clear()
+
+quantidade_de_pessoas_cadastradas = list()
+
+for i in pessoas:
+    quantidade_de_pessoas_cadastradas.append(i[0])
+
+print('-='*30)
+print(f'As quantidade de pessoas da lista: {len(quantidade_de_pessoas_cadastradas)}')
+numero_para_dividir = len(pessoas) // 2
+pessoas_mais_pesadas = list()
+
+for i in range(numero_para_dividir):
+    maior_peso = 0
+    for j in pessoas:
+        if maior_peso == 0:
+            maior_peso = j[1]
+        if j[1] > maior_peso:
+            maior_peso = j[1]      
+    for z, x in enumerate(pessoas):
+        if x[1] == maior_peso:
+            pessoas_mais_pesadas.append(x)
+            pessoas.pop(z)
+
+print('-='*30)
+print('Pessoas mais pesadas:')
+for i in pessoas_mais_pesadas:
+    print(f'{i[0]} pesando {i[1]}kg')
+
+print('-='*30)
+print('Pessoas mais leves:')
+for i in pessoas:
+    print(f'{i[0]} pesando {i[1]}kg')
